ee43ee439b2ed92d09a946df8febc3f712599ef2
tjquillan <tjquillan@gmail.com>
2023-10-18 09:52:37 -0400

    Initial table workings

--- src/Leaderboard.tsx
+++ src/Leaderboard.tsx
@@ -1,9 +1,19 @@
 import axios from "axios";
 import { useCallback, useState } from "react";
-import { Heading, Text } from "@chakra-ui/react";
+import {
+  Heading,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 import { useQuery } from "@tanstack/react-query";
 import {
   createColumnHelper,
+  flexRender,
   getCoreRowModel,
   getSortedRowModel,
   useReactTable,
@@ -78,27 +88,32 @@ function fetchLeaderboardEntries(
     .then((map) => Array.from(map.values()));
 }
 
-function LeaderboardQuery() {
-    
-}
-
 function Leaderboard({ hole, language }: LeaderboardProps) {
   const solutionsQueryFn = useCallback((): Promise<LeaderboardEntry[]> => {
     return fetchLeaderboardEntries(hole, language);
   }, [hole, language]);
-  const { isLoading, data, isSuccess } = useQuery({
+  const { isPending, data, isSuccess } = useQuery({
     queryKey: [hole, language],
     queryFn: solutionsQueryFn,
+    initialData: [],
   });
 
   const table = useReactTable({
     columns,
-    data: data || [],
+    data: data,
     getCoreRowModel: getCoreRowModel(),
     getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting: [
+        {
+          id: "chars",
+          desc: false,
+        },
+      ],
+    },
   });
 
-  if (isLoading) {
+  if (isPending) {
     return <Text>Loading...</Text>;
   }
 
@@ -106,7 +121,41 @@ function Leaderboard({ hole, language }: LeaderboardProps) {
     return <Text>Error!</Text>;
   }
 
-  return <></>;
+  return (
+    <Table>
+      <Thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <Tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => {
+              const meta = header.column.columnDef.meta;
+              return (
+                <Th key={header.id} isNumeric={meta?.isNumeric}>
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+                </Th>
+              );
+            })}
+          </Tr>
+        ))}
+      </Thead>
+      <Tbody>
+        {table.getRowModel().rows.map((row) => (
+          <Tr key={row.id}>
+            {row.getVisibleCells().map((cell) => {
+              const meta = cell.column.columnDef.meta;
+              return (
+                <Td key={cell.id} isNumeric={meta?.isNumeric}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </Td>
+              );
+            })}
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
+  );
 }
 
 export default Leaderboard;
