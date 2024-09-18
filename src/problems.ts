73f024eeaf02c67a9fff1dda35f19553bb79aea2
tim <tperr916@yahoo.com>
2024-09-18 13:13:24 -0400

    reset users and problems

--- src/problems.ts
+++ src/problems.ts
@@ -1,14 +1,5 @@
 export const problems = [
   "fizz-buzz",
-  "evil-numbers",
-  "diamonds",
-  "catalans-constant",
-  "rock-paper-scissors-spock-lizard",
-  "hexdump",
-  "intersection",
-  "fibonacci",
-  "day-of-week",
-  "ascii-table",
 ];
 export const bonus_problems = ["game-of-life"];
 export const langs = ["java", "python", "c", "cpp"];
