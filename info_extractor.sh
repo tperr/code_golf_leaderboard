87f43aafd2e02c39deaa123d18404d0d835e80d0
tperr <tperr916@yahoo.com>
2024-09-28 21:15:27 -0400

    slightly condensed

--- info_extractor.sh
+++ info_extractor.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 # this could be more than one line but like no
 # requires advanced search search to be completed, then copy the entire table to banweb.txt
-grep -v -E "L[0-9]" banweb.txt | grep -v -E "6999|6990|6091|5999|5990|4099" | awk -F "\t" '{print $3 $4 $8 $14}' | uniq -u > classinfo.txt
+# filters out not relevant classes and labs
+grep -v -E "L[0-9]|6999|6990|6091|5999|5990|4099" banweb.txt | awk -F "\t" '{print $3 $4 $8 $14}' | uniq -u > classinfo.txt
