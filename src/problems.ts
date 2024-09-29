5366f7f7033a66b79da4f979a40a916595ed1f72
tperr <tperr916@yahoo.com>
2024-09-29 15:57:01 -0400

    keeping old problems

--- src/problems.ts
+++ src/problems.ts
@@ -1,5 +1,20 @@
 export const problems = [
-  "fizz-buzz",
+
 ];
-export const bonus_problems = ["game-of-life"];
+export const bonus_problems = [];
+
+
+export const problems_2023 = [
+  "fizz-buzz",
+  "evil-numbers",
+  "diamonds",
+  "catalans-constant",
+  "rock-paper-scissors-spock-lizard",
+  "hexdump",
+  "intersection",
+  "fibonacci",
+  "day-of-week",
+  "ascii-table",
+];
+export const bonus_problems_2023 = ["game-of-life"];
 export const langs = ["java", "python", "c", "cpp"];
