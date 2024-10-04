4140376248c4a20737c4ca1c3fc506e85c2d56f6
tim <tperr916@yahoo.com>
2024-10-04 18:46:03 -0400

    updating golfers and stuff

--- src/golfers.ts
+++ src/golfers.ts
@@ -1,6 +1,23 @@
 const golfers = new Set([
     "tperr",
     "HPWiz",
+    "gthayden23",
+    "Colinwat",
+    "RaVoTTi",
+    "That1GuyWhoSucks",
+    "chwoods1",
+    "WilczynskiCMTU",
+    "mtchkelly",
+    "AndrewKentavr",
+    "robbyj1388",
+    "onsagere",
+    "jccummi1",
+    "sanjeev29".
+    "AndrewKentavr",
+    "spacecatplayz",
+    "TheBrokenRail",
+    "PaulBadour",
+    "rohith-kola",
 ]);
 
 export default golfers;
